75c3d15f1d45f1935b1fb6d19b6eddc82ec8aead
Svetlana Koryakina <Koryakina.Svetlana@outlook.com>
2019-10-25 22:48:25 +0500

    убрала лишние комментарии

--- src/components/card/card.e2e.test.js
+++ src/components/card/card.e2e.test.js
@@ -7,17 +7,13 @@ Enzyme.configure({adapter: new Adapter()});
 
 it(`Check click on card header`, () => {
   const clickHandler = jest.fn();
-  const card = shallow(<Card // mount (<App />);
+  const card = shallow(<Card
     film={`1`}
     onHeaderClick={clickHandler}
   />);
 
   const header = card.find(`h3`);
   header.simulate(`click`); // click on h3
-  // simulate(`click`, { preventDefault() {} }) // если mount
-
-  // const preventDefault = jest.fn();
-  // header.simulate(`click`, {preventDefault});
-
+ 
   expect(clickHandler).toHaveBeenCalledTimes(1);
 });

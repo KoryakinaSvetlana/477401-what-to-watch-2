2b376a7393bf8afb83dab080a54e97b9f9152ac7
Svetlana Koryakina <Koryakina.Svetlana@outlook.com>
2019-11-13 22:56:24 +0500

    тестирование роутера

--- src/components/app/app.test.jsx
+++ src/components/app/app.test.jsx
@@ -3,10 +3,10 @@ import renderer from "react-test-renderer";
 import {Route} from 'react-router';
 import Enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import {BrowserRouter as Router} from "react-router-dom";
 
 import App from "./app.jsx";
-import NotFound from "../not-found/not-found.jsx";
+import Details from "components/details/details";
+// import NotFound from "components/not-found/not-found";
 
 Enzyme.configure({adapter: new Adapter()});
 
@@ -21,15 +21,17 @@ it(`App correctly renders`, () => {
 
 
 it(`invalid path should redirect to NotFound page`, () => {
-  const wrapper = shallow(<Router><App
+  const wrapper = shallow(<App
     films={[{id: 1, name: `1`}, {id: 2, name: `2`}, {id: 3, name: `3`}]}
-  /></Router>);
+  />);
   const pathMapTest = wrapper.find(Route).reduce((pathMap, route) => {
     const routeProps = route.props();
     pathMap[routeProps.path] = routeProps.component;
     return pathMap;
   }, {});
 
-  expect(pathMapTest[`/somepath`]).toBe(NotFound);
+  // expect(pathMapTest[`/somepath`]).toBe(NotFound);
+
+  expect(pathMapTest[`/films/2`]).toBe(Details);
 });
 
